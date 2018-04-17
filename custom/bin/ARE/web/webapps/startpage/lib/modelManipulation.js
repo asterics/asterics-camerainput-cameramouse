c7726c18a88c9405243247d068ced89aab0e5933
Martin Deinhofer <martin.deinhofer@technikum-wien.at>
2018-04-17 15:43:37 +0200

    Big changes which should have been committed seperately :-)
    Removed need of defining widgetIdToPropertyKeyMap index.html
     - Now you can use an HTML5 data attribute
    (data-asterics-model-binding-1,...) to define a binding between a
    widgetId and an AsTeRICS model property
    
     example:
     <input title="Mouse speed" type="range" id="speed" value="60" min="1"
    max="100" data-asterics-model-binding-1='{"componentKey":
    "Slider.1","propertyKey": "default"}'>
    
     * moved almost all functions to webAppUtils.js and modelManipulation.js
     * renamed default callbacks to defaultSuccessCallback and
    defaulErrorCallback
     * all functions with server requests now support optional
    successCallback and errorCallback functionality.
    
     Added JSdoc style comments http://usejsdoc.org/index.html

--- custom/bin/ARE/web/webapps/startpage/lib/modelManipulation.js
+++ custom/bin/ARE/web/webapps/startpage/lib/modelManipulation.js
@@ -1,3 +1,19 @@
+/**
+ * @file
+ * This Javascript library provides functions that simplify manipulation of an AsTeRICS model in XML representation.
+ * The lib provides functions for setting and getting a property value.
+ * 
+ * @author Martin Deinhofer
+ * @version 0.1
+ */
+
+/**
+ * Sets the property componentKey.propertyKey to the given propertyValue.
+ * @param {string} componentKey - The component instance id.
+ * @param {string} propertyKey - The property key of the component.
+ * @param {string} propertyValue - The new value.
+ * @param {string} xmlDoc - The XML model as string to be updated. 
+ */
 function setPropertyValueInXMLModel(componentKey, propertyKey, propertyValue, xmlDoc) {
 	var commandPanel=xmlDoc.getElementsByTagName('component');
 
@@ -12,7 +28,7 @@ function setPropertyValueInXMLModel(componentKey, propertyKey, propertyValue, xm
 				var curProperty=commandPanelProperties.item(j);
 				if(curProperty.getAttribute("name")==propertyKey) {
 					curProperty.setAttribute("value",propertyValue);
-					console.log("Property ["+componentKey+"."+propertyKey+"="+propertyValue+"] set");											
+					//console.log("Property ["+componentKey+"."+propertyKey+"="+propertyValue+"] set");											
 					found=true;
 				}
 			}					
@@ -23,6 +39,13 @@ function setPropertyValueInXMLModel(componentKey, propertyKey, propertyValue, xm
 	}
 }
 
+/**
+ * Gets the property value of property componentKey.propertyKey.
+ * @param {string} componentKey - The component instance id.
+ * @param {string} propertyKey - The property key of the component.
+ * @param {string} xmlDoc - The XML model as string to be updated. 
+ * @returns {string} The property value.
+ */
 function getPropertyValueFromXMLModel(componentKey, propertyKey, xmlDoc) {
 	var commandPanel=xmlDoc.getElementsByTagName('component');
 
@@ -36,7 +59,7 @@ function getPropertyValueFromXMLModel(componentKey, propertyKey, xmlDoc) {
 				var curProperty=commandPanelProperties.item(j);
 				if(curProperty.getAttribute("name")==propertyKey) {
 					var propVal=curProperty.getAttribute("value");
-					console.log("Property ["+componentKey+"."+propertyKey+"="+propVal+"]");
+					//console.log("Property ["+componentKey+"."+propertyKey+"="+propVal+"]");
 					return propVal;
 				}
 			}					
@@ -46,6 +69,11 @@ function getPropertyValueFromXMLModel(componentKey, propertyKey, xmlDoc) {
 	return undefined;
 }
 
+/**
+ * Converts the given XML document object to an XML string.
+ * @param {Document} xmlData - The XML document object. 
+ * @returns {string} The XML document as string.
+ */
 function xmlToString(xmlData) { 
     var xmlString;
     //IE
--- custom/bin/ARE/web/webapps/startpage/lib/webAppUtils.js
+++ custom/bin/ARE/web/webapps/startpage/lib/webAppUtils.js
@@ -1,22 +1,27 @@
-<!-- Here AsTERICS part starts -->
-/* This is an example of how to use the ARE Javascipt framework for the communication
-	with the ARE Restfull Services.
-	
-	The location of the server should be defined with the 'setBaseURI(<url>)' method.
-	
-	A success-callback function and an error-callback function should be passed as an argument
-	for every function.
-*/
-
-//Set the base URI of the running ARE instance.
-setBaseURI("http://localhost:8081/rest/");
+/**
+ * @file
+ * This Javascript library provides functions that simplify the creation of web-based user interfaces interacting with an AsTeRICS model.
+ * The lib provides functions for downloading files from a webserver, widget <-> model synchronization, up/download/start models, setting/getting properties of deployed model.
+ *   
+ * @requires jquery-3.2.1.min.js (or maybe lower versions also)
+ * @requires JSmap.js (AsTeRICS 3.0)
+ * @requires areCommunicator.js (AsTeRICS 3.0)
+ * 
+ * @author Martin Deinhofer
+ * @version 0.1
+ */
 
 /**
  * Loads a file hosted on the same webserver as this file and returns the contents as plain text.
  * @param {string} relFilePath - The path to the file relative to http://<location.origin>/subpath/.
- * @param {function(fileContentsAsString)} successCallback - The callback function to be called with the file contents.
+ * @param {function(fileContentsAsString)} [successCallback=defaultSuccessCallback] - The callback function to be called with the file contents. 
+ * @param {function(HTTPstatus, errorMessage)} [errorCallback=defaultErrorCallback]- Callback function in case of an error.
  */
-function loadFileFromWebServer(relFilePath, successCallback) {
+function loadFileFromWebServer(relFilePath, successCallback, errorCallback) {
+	//assign default callback functions if none was provided.
+	successCallback=getSuccessCallback(successCallback);
+	errorCallback=getErrorCallback(errorCallback);	
+	
 	var httpReq = new XMLHttpRequest();
 	console.log("location.origin: "+location.origin);
 	
@@ -50,10 +55,15 @@ function loadFileFromWebServer(relFilePath, successCallback) {
 
 /**
  * Deploys a model file hosted on the same webserver as this file to a running ARE instance e.g. on localhost.
- * @param {string} relFilePath - The path to the file relative to http://<location.origin>/subpath/.
- * @param {function(data, HTTPstatus)} successCallback - The success callback function.
+ * @param {string} relFilePath - The path to the file relative to http://location.origin/subpath/.
+ * @param {function(data, HTTPstatus)} [successCallback=defaultSuccessCallback] - The callback function to be called with the file contents. 
+ * @param {function(HTTPstatus, errorMessage)} [errorCallback=defaultErrorCallback]- Callback function in case of an error. 
  */
-function deployModelFromWebserver(relFilePath, successCallback) {
+function deployModelFromWebserver(relFilePath, successCallback, errorCallback) {
+	//assign default callback functions if none was provided.
+	successCallback=getSuccessCallback(successCallback);
+	errorCallback=getErrorCallback(errorCallback);	
+	
 	loadFileFromWebServer(relFilePath, 
 		function(modelInXML) {				
 			uploadModel(successCallback, errorCallback, modelInXML);
@@ -63,7 +73,7 @@ function deployModelFromWebserver(relFilePath, successCallback) {
 /**
  * Deploys a model file hosted on the same webserver as this file to a running ARE instance e.g. on localhost.
  * Additionally applies the property settings in the given propertyMap and if successful starts the model.
- * @param {string} relFilePath - The path to the file relative to http://<location.origin>/subpath/.
+ * @param {string} relFilePath - The path to the file relative to http://location.origin/subpath/.
  * @param {string } propertyMap - A JSON string of property keys and values (see function setRuntimeComponentProperties) in the format: 
  
  {
@@ -76,8 +86,14 @@ function deployModelFromWebserver(relFilePath, successCallback) {
       "key_2_2":"val_2_2"
    }
 }
+ * @param {function(data, HTTPstatus)} [successCallback=defaultSuccessCallback] - The callback function to be called with the file contents. 
+ * @param {function(HTTPstatus, errorMessage)} [errorCallback=defaultErrorCallback]- Callback function in case of an error. 
  */
-function deployModelFromWebserverApplySettingsAndStartModel(relFilePath, propertyMap) {
+function deployModelFromWebserverApplySettingsAndStartModel(relFilePath, propertyMap, successCallback, errorCallback) {
+	//assign default callback functions if none was provided.
+	successCallback=getSuccessCallback(successCallback);
+	errorCallback=getErrorCallback(errorCallback);	
+	
 	deployModelFromWebserver(relFilePath,
 		function() {
 			setRuntimeComponentProperties(			
@@ -88,7 +104,7 @@ function deployModelFromWebserverApplySettingsAndStartModel(relFilePath, propert
 					}
 					console.log('The following properties could be set: '+data);
 					
-					startModel(Empty_successCallback,errorCallback);
+					startModel(successCallback,errorCallback);
 				}, 
 				errorCallback, propertyMap);
 		});
@@ -96,24 +112,113 @@ function deployModelFromWebserverApplySettingsAndStartModel(relFilePath, propert
 
 /**
  * Stores the file hosted on the same webserver as this file at a running ARE instance e.g. on localhost and the given relFilePathARE.
- * @param {string} relFilePath - The path of the file relative to http://<location.origin>/subpath/.
+ * @param {string} relFilePath - The path of the file relative to http://location.origin/subpath/.
  * @param {string} relFilePathARE - The store path of the file on the ARE relative to ARE/data.
- * @param {function(data, HTTPstatus)} successCallback - The success callback function.
+ * @param {function(data, HTTPstatus)} [successCallback=defaultSuccessCallback] - The callback function to be called with the file contents. 
+ * @param {function(HTTPstatus, errorMessage)} [errorCallback=defaultErrorCallback]- Callback function in case of an error. 
  */
-function storeFileFromWebserverOnARE(relFilePath, relFilePathARE, successCallback) {
+function storeFileFromWebserverOnARE(relFilePath, relFilePathARE, successCallback, errorCallback) {
+	//assign default callback functions if none was provided.
+	successCallback=getSuccessCallback(successCallback);
+	errorCallback=getErrorCallback(errorCallback);	
+	
 	loadFileFromWebServer(relFilePath, 
 		function(fileContentsAsString) {
 			storeData(successCallback, errorCallback, relFilePathARE, fileContentsAsString);
 		});
 }
 
+/**
+ * Download the model file (modelFilePathOnWebserver) hosted on a web server, apply all settings to the XML model, 
+ * which have a defined binding (data-asterics-model-binding-1,...) to a model property and finally start the model.
+ * @param {string} modelFilePathOnWebserver - The path of the file relative to http://location.origin/subpath/.
+ * @param {function(data, HTTPstatus)} [successCallback=defaultSuccessCallback] - The callback function to be called with the file contents. 
+ * @param {function(HTTPstatus, errorMessage)} [errorCallback=defaultErrorCallback]- Callback function in case of an error.   
+*/		
+function applySettingsInXMLModelAndStart(modelFilePathOnWebserver, successCallback, errorCallback) {
+	//assign default callback functions if none was provided.
+	successCallback=getSuccessCallback(successCallback);
+	errorCallback=getErrorCallback(errorCallback);	
+	
+	loadFileFromWebServer(modelFilePathOnWebserver, function(modelInXML){
+		modelInXML=updateModelPropertiesFromWidgets(modelInXML);
+		
+		//Finally upload and start modified model.
+		uploadModel(function(data, HTTPstatus) {					
+			startModel(function(data,HTTPStatus) {
+				successCallback(data, HTTPStatus);
+			},errorCallback);					
+		}, errorCallback, modelInXML);
+	});	
+}
+
+/**
+ * Download the currently deployed model from the ARE and update all widgets with the property values in the XML model, 
+ * which have a defined binding (data-asterics-model-binding-1,...) to a model property.
+ * @param {string} modelFilePathOnWebserver - The path of the file relative to http://location.origin/subpath/.
+ * @param {function(data, HTTPstatus)} [successCallback=defaultSuccessCallback] - The callback function to be called with the file contents. 
+ * @param {function(HTTPstatus, errorMessage)} [errorCallback=defaultErrorCallback]- Callback function in case of an error.   
+*/		
+function updateWidgetsFromDeployedModel(successCallback, errorCallback) {
+	//assign default callback functions if none was provided.
+	successCallback=getSuccessCallback(successCallback);
+	errorCallback=getErrorCallback(errorCallback);
+
+	downloadDeployedModel(function(data, HTTPStatus) {
+		//TODO: Check if the modelName is identical to the modelName of the template model, otherwise we should not
+		//update the widgets from a wrong model.
+		updateWidgetsFromModelProperties(data);
+	},successCallback);
+}
+
+/**
+ * Applies the settings to the model modelFilePathOnWebserver and saves it as autostart model on the ARE.
+ * @param {string} modelFilePathOnWebserver - The path of the file relative to http://location.origin/subpath/.
+ * @param {function(data, HTTPstatus)} [successCallback=defaultSuccessCallback] - The callback function to be called with the file contents. 
+ * @param {function(HTTPstatus, errorMessage)} [errorCallback=defaultErrorCallback]- Callback function in case of an error.   
+*/		
+function saveSettingsAsAutostartModel(modelFilePathOnWebserver, successCallback, errorCallback) {
+	//assign default callback functions if none was provided.
+	if(typeof successCallback !== 'function') {
+		successCallback=function(data,HTTPStatus) {
+			alert('Successfully saved settings as autostart!');
+		}
+	}
+	errorCallback=getErrorCallback(errorCallback);
+	
+	loadFileFromWebServer(modelFilePathOnWebserver, function(modelInXML){
+		modelInXML=updateModelPropertiesFromWidgets(modelInXML);
+		storeModel(successCallback,errorCallback,'autostart.acs',modelInXML);
+	});			
+}
+
+/** 
+ * @const {string}
+ * @description HTML5 data attribute to define binding to AsTeRICS model 
+ */
+var dataAttrAstericsModelBinding="data-asterics-model-binding-1";
+
+/**
+ * Updates the property values of the given modelInXML string with the currently set widget values.
+ * @param {string} modelInXML - The AsTeRICS model as XML string.
+ * @returns {string} The modified XML model as string.
+ */
 function updateModelPropertiesFromWidgets(modelInXML) {
+	widgetIdToPropertyKeyMap=generateWidgetIdToPropertyKeyMap();
+	console.log("Updating "+widgetIdToPropertyKeyMap.length+" widgets from model properties.");
+	
 	//parse template modelInXML to document object
 	var xmlDoc = $.parseXML( modelInXML );
 	
 	//Update property values with values of input widgets.
 	for(var i=0;i<widgetIdToPropertyKeyMap.length;i++) {
-		setPropertyValueInXMLModel(widgetIdToPropertyKeyMap[i]["componentKey"],widgetIdToPropertyKeyMap[i]["propertyKey"],$(widgetIdToPropertyKeyMap[i]["widgetId"]).val(),xmlDoc);				
+		var widgetVal=$(widgetIdToPropertyKeyMap[i]["widgetId"]).val();
+		if (typeof widgetVal != 'undefined') {
+			console.log("Updating modelProperty <"+widgetIdToPropertyKeyMap[i]["componentKey"]+"."+widgetIdToPropertyKeyMap[i]["propertyKey"]+"="+widgetVal+">");
+			setPropertyValueInXMLModel(widgetIdToPropertyKeyMap[i]["componentKey"],widgetIdToPropertyKeyMap[i]["propertyKey"],widgetVal,xmlDoc);
+		} else {
+			console.log("widgetId <"+widgetIdToPropertyKeyMap[i]["widgetId"]+"=undefined>");
+		}
 	}
 	
 	//Convert back XML document to XML string.
@@ -121,21 +226,91 @@ function updateModelPropertiesFromWidgets(modelInXML) {
 	return modelInXML;
 }
 
+/**
+ * Updates the widgets (HTML input elements) with the property values of the given modelInXML string.
+ * @param {string} modelInXML - The AsTeRICS model as XML string.
+ */
 function updateWidgetsFromModelProperties(modelInXML) {
+	widgetIdToPropertyKeyMap=generateWidgetIdToPropertyKeyMap();
+	console.log("Updating "+widgetIdToPropertyKeyMap.length+" widgets from model properties.");
+	
 	//parse template modelInXML to document object
 	var xmlDoc = $.parseXML( modelInXML );
 	
 	//Update property values with values of input widgets.
 	for(var i=0;i<widgetIdToPropertyKeyMap.length;i++) {
 		var propVal=getPropertyValueFromXMLModel(widgetIdToPropertyKeyMap[i]["componentKey"],widgetIdToPropertyKeyMap[i]["propertyKey"],xmlDoc);
-		if(propVal!=undefined) {
-			console.log("Updating widget["+widgetIdToPropertyKeyMap[i]["widgetId"]+"]: "+propVal);
+		if(typeof propVal != 'undefined') {
+			console.log("Updating widget <"+widgetIdToPropertyKeyMap[i]["widgetId"]+"="+propVal+">");
 			$(widgetIdToPropertyKeyMap[i]["widgetId"]).val(propVal);					
 		}
-	}			
+	}	
 }
 
+/**
+ * Generates an array describing the bindings between all widgetIds (id of HTML5 input tag) and their respective model properties.
+ * @returns {Array} - Array with Javascript object elements. 
+ */
+function generateWidgetIdToPropertyKeyMap() {
+	var widgetIdToPropertyKeyMap=[];
+	var widgetList=$("["+dataAttrAstericsModelBinding+"]");
+	for(var i=0;i<widgetList.length;i++) {
+		var bindings=$(widgetList[i]).data();
+		for(binding in bindings) {
+			var bindingObj=	{
+				widgetId:"#"+$(widgetList[i]).attr('id'),
+				componentKey:bindings[binding]["componentKey"],
+				propertyKey:bindings[binding]["propertyKey"]
+			}
+			widgetIdToPropertyKeyMap.push(bindingObj);
+		}
+	}
+	return widgetIdToPropertyKeyMap;
+}
+
+/**
+ * Returns a valid callback function - either successCallback if != undefined or {defaultSuccessCallback}.
+ * @param {function(data, HTTPstatus)} [successCallback=defaultSuccessCallback] - The callback function to be used.
+ * @returns {function(data, HTTPstatus)} - Either successCallback or defaultSuccessCallback.
+*/		
+function getSuccessCallback(successCallback) {
+	if(typeof successCallback !== 'function') {
+		return defaultSuccessCallback;
+	}
+	return successCallback;	
+}
+
+/**
+ * Returns a valid callback function - either errorCallback if != undefined or {defaultErrorCallback}.
+ * @param {function(HTTPstatus, errorMessage)} [errorCallback=defaultErrorCallback]- The callback function to be used.
+ * @returns {function(HTTPstatus, errorMessage)} - Either errorCallback or defaultErrorCallback.
+*/		
+function getErrorCallback(errorCallback) {
+	if(typeof errorCallback !== 'function') {
+		return defaultErrorCallback;
+	}
+	return errorCallback;
+}
+
+
 /* generic callback handler */
-function Empty_successCallback(data, HTTPstatus) {}
-function errorCallback(HTTPstatus, AREerrorMessage) { alert("An error occured: "+AREerrorMessage+"\nPlease ensure to install AsTeRICS and start the ARE!"); }
+/**
+ * This is the default success callback. 
+ * By default nothing is done.
+ *
+ * @callback defaultSuccessCallback
+ * @param {data} - response text or message.
+ * @param {HTTPstatus} - HTTP status code if applicable.
+ */
+function defaultSuccessCallback(data, HTTPstatus) {}
+/**
+ * This is the default error callback. 
+ * By default an error dialog (alert) is opened.
+ *
+ * @callback defaultErrorCallback
+ * @param {HTTPstatus} - HTTP status code if applicable.
+ * @param {errorMessage} - The error message to be shown.
+
+ */
+function defaultErrorCallback(HTTPstatus, errorMessage) { alert("An error occured: "+errorMessage+"\nPlease ensure to install AsTeRICS and start the ARE!"); }
 
